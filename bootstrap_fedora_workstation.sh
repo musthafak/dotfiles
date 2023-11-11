897e27304cffb3d300781dff5531d4fb5eca3ced
Musthafa Kariyaparambil <36440757+musthafak@users.noreply.github.com>
2023-11-11 13:11:56 +0530

    remove unused packages on fedora

--- bootstrap_fedora_workstation.sh
+++ bootstrap_fedora_workstation.sh
@@ -6,23 +6,16 @@ set -e
 [[ ! -f /etc/fedora-release ]] && echo "ERROR: Only supports fedora!" && exit 1
 
 # Enable RPM Fusion and VS Code repo.
-sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
+sudo dnf install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
     https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
-sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
-sudo sh -c 'echo -e "[code]
-name=Visual Studio Code
-baseurl=https://packages.microsoft.com/yumrepos/vscode
-enabled=1
-gpgcheck=1
-gpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
 
 # Install multi-media packages
 sudo dnf update --refresh
-sudo dnf groupupdate -y multimedia --setop="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin
-sudo dnf groupupdate -y sound-and-video
+sudo dnf groupupdate -y --skip-broken multimedia --setop="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin
+sudo dnf groupupdate -y --skip-broken sound-and-video
 
 # Others
-sudo dnf install -y stow htop zsh code lsd google-chrome-stable alacritty moby-engine
+sudo dnf install -y stow htop zsh lsd alacritty
 
 # VA-API
 sudo dnf install -y libva libva-utils intel-media-driver intel-gpu-tools gstreamer1-vaapi
@@ -35,7 +28,7 @@ sudo usermod -s /usr/bin/zsh $USER
 
 # Install flatpak apps from flathub
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-flatpak install com.mattjakeman.ExtensionManager de.haeckerfelix.Fragments org.gnome.meld \
+flatpak install com.mattjakeman.ExtensionManager de.haeckerfelix.Fragments \
     com.github.rafostar.Clapper de.haeckerfelix.Shortwave io.bassi.Amberol
 
 # Create a workspace directory
